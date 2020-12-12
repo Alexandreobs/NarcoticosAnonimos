ab92a26e1efcc22ab015e930f5d631b88876e05d
alexandre.silva <Alex@75321>
2020-12-12 12:48:07 -0300

    implementado função de compartilhar texto

--- app/src/main/java/com/alexandreobsilva/a12passosnamauricio/view/activitys/DetalheActivity.kt
+++ app/src/main/java/com/alexandreobsilva/a12passosnamauricio/view/activitys/DetalheActivity.kt
@@ -1,7 +1,17 @@
 package com.alexandreobsilva.a12passosnamauricio.view.activitys
 
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.alexandreobsilva.a12passosnamauricio.R
 import kotlinx.android.synthetic.main.activity_detalhe.*
 import com.alexandreobsilva.a12passosnamauricio.model.pojos.PassosTradicoesConceitos
@@ -12,13 +22,36 @@ class DetalheActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhe)
 
-        val recebidos = intent.extras?.getSerializable("Key10") as PassosTradicoesConceitos
+        recebeAsParadas()
 
+        buttonEntendi.setOnClickListener { finish() }
+        btn_compartilhar.setOnClickListener { copartilharTextos() }
+
+    }
+
+    fun recebeAsParadas(){
+        val recebidos = intent.extras?.getSerializable("Key10") as PassosTradicoesConceitos
         text_view_detalhe_titulo.text = recebidos.titulo
         textViewDes.text = recebidos.textodetalhe
 
-        buttonEntendi.setOnClickListener {
-            finish()
+    }
+
+    fun copartilharTextos() {
+      val recebidos = intent.extras?.getSerializable("Key10") as PassosTradicoesConceitos
+        val text1 = recebidos.textodetalhe
+        val text2 = recebidos.titulo
+
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Texto compartilhado atrávez do app Vitória Anônima\n\n feito por https://www.linkedin.com/in/alexandreobs/\n\n" +
+                  text2 + "\n\n" + text1 )
+            type = "text/plain"
         }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
     }
 }
+
+
+
